a6226a1d625f25a70f0569c71ed9c94a58a32143
Mikkel Gadegaard <mgadegaard@us6c7e67bb6586.am.sony.com>
2024-11-12 16:33:18 -0800

    Changed header Content-Type back to html

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting the serfver on :3000...")
+	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
