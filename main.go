144ca354fd84bfa318edf9bcb5c1bbcebca92b76
Mikkel Gadegaard <mgadegaard@us6c7e67bb6586.am.sony.com>
2024-11-14 23:25:24 -0800

    Exercise solved

--- main.go
+++ main.go
@@ -15,12 +15,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch with us, send an email to <a href=\"mailto:mikkel@skewedbytes.com\">mikkel@skewedbytes.com</a>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ</h1><p>Q: Is there a free version?</br>A: Yes! We offer a free trial for 30 days on any paid plans.</p><p>Q: What are your support hours?</br>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p><p>Q: How do I contact support?</br>A: Email us @ support@lenslocked.com</p>")
+}
+
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	}
@@ -41,9 +48,10 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 // }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
-	// var router http.HandlerFunc = pathHandler
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// http.HandleFunc()
+	var router http.HandlerFunc = pathHandler
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
