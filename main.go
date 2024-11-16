f2c10a26ccce1cf48ca67f7297bceba12716283a
Mikkel Gadegaard <mgadegaard@us6c7e67bb6586.am.sony.com>
2024-11-15 16:27:35 -0800

    Using Chi as router

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,21 +19,21 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ</h1><p>Q: Is there a free version?</br>A: Yes! We offer a free trial for 30 days on any paid plans.</p><p>Q: What are your support hours?</br>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p><p>Q: How do I contact support?</br>A: Email us @ support@lenslocked.com</p>")
+	fmt.Fprint(w, "<h1>FAQ</h1><p><ul>Q: Is there a free version?</br>A: Yes! We offer a free trial for 30 days on any paid plans.</p><p>Q: What are your support hours?</br>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p><p>Q: How do I contact support?</br>A: Email us @ support@lenslocked.com</p><ul>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-	}
-}
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/faq":
+// 		faqHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 	}
+// }
 
 // type Router struct {
 // }
@@ -48,10 +50,10 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 // }
 
 func main() {
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-	// http.HandleFunc()
-	var router http.HandlerFunc = pathHandler
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
