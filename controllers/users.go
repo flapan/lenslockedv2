2bd742b6e0a34065227c3546d0bfbe158f884da2
Mikkel Gadegaard <mikkel@skewedbytes.com>
2024-12-19 17:00:17 -0800

    BEginning work n parsing data from signup form in Users controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,4 +13,22 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	// var data struct {
+	// 	Email    string
+	// 	Password string
+	// }
+	// data.Email = r.FormValue("email")
+	// data.Password = r.FormValue("password")
+	// fmt.Println(data)
+	// u.Templates.New.Execute(w, data)
 }
