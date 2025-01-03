c2213ec7b3b65c5b7fcaef5b1f32648ea2f40b43
Mikkel Gadegaard <mikkel@skewedbytes.com>
2025-01-03 11:00:41 -0800

    Added UserService to Users

--- models/users.go
+++ models/users.go
@@ -1,9 +1,15 @@
 package models
 
+import "database/sql"
+
 type User struct {
 	ID        int
 	Username  string
 	Password  string
 	Email     string
 	CreatedAt string
+}
+
+type UserService struct {
+	DB *sql.DB
 }
