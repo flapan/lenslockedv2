c71d46709af1bd0d87501dfef9098be70c87f00d
Mikkel Gadegaard <mikkel@skewedbytes.com>
2025-01-03 12:33:52 -0800

    Adjusted user model and added create user function

--- models/sql/users.sql
+++ models/sql/users.sql
@@ -1,6 +1,5 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
     password TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- models/users.go
+++ models/users.go
@@ -1,10 +1,15 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID        int
-	Username  string
 	Password  string
 	Email     string
 	CreatedAt string
@@ -12,4 +17,30 @@ type User struct {
 
 type UserService struct {
 	DB *sql.DB
+}
+
+func (us *UserService) Create(password, email string) (*User, error) {
+	email = strings.ToLower(email)
+	hashshedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+
+		return nil, fmt.Errorf("Create user: %W", err)
+	}
+	passwordHash := string(hashshedBytes)
+
+	user := User{
+		Email:    email,
+		Password: passwordHash,
+	}
+
+	row := us.DB.QueryRow(`
+	INSERT INTO users (password, email)
+	VALUES ($1, $2) RETURNING id`, passwordHash, email)
+
+	err = row.Scan(&user.ID)
+
+	if err != nil {
+		return nil, fmt.Errorf("Create user: %w", err)
+	}
+	return &user, nil
 }
