137ff112332469871776329ffd76651420f06f2c
Mikkel Gadegaard <mikkel@skewedbytes.com>
2025-01-06 13:14:57 -0800

    Added sign in template and hooked it up to sign in links and controller, missing authentication, so not actually verifying that user is good to sign in yet

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ func main() {
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "tailwind.gohtml", "signup.gohtml"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "tailwind.gohtml", "signin.gohtml"))
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
