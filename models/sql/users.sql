dfb2102186abeb04aa99fc33c7e2b79cd82bf8c6
Mikkel Gadegaard <mikkel@skewedbytes.com>
2025-03-21 14:33:04 -0700

    removed sql files that are now unused because of using goose migrations

--- models/sql/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    password TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
--- TODO: Change password to password_hash
